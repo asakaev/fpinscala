4d7377c7553005b68da0d9204bab3e84e7d70fd2
Akhtyam Sakaev <akhtyam.sakaev@iqoption.com>
2017-12-27 13:39:02 +0300

    fib tailrec

--- src/main/scala/fpinscala/Exercises.scala
+++ src/main/scala/fpinscala/Exercises.scala
@@ -1,8 +1,14 @@
 package fpinscala
 
+import scala.annotation.tailrec
+
 trait Exercises {
 
-  def fib(n: Int): Int =
-    if (n <= 1) 1 else fib(n - 1) + fib(n - 2)
+  def fib(n: Int): Int = {
+    @tailrec def fibAux(a: Int, b: Int, n: Int): Int =
+      if (n > 0) fibAux(b, a + b, n - 1) else a
+
+    fibAux(0, 1, n)
+  }
 
 }
--- src/test/scala/fpinscala/ExercisesSuite.scala
+++ src/test/scala/fpinscala/ExercisesSuite.scala
@@ -4,9 +4,11 @@ import org.scalatest.FunSuite
 
 class ExercisesSuite extends FunSuite with Exercises {
   test("fib") {
-    assert(fib(0) == 1)
+    assert(fib(0) == 0)
     assert(fib(1) == 1)
-    assert(fib(5) == 8)
-    assert(fib(6) == 13)
+    assert(fib(2) == 1)
+    assert(fib(3) == 2)
+    assert(fib(4) == 3)
+    assert(fib(20) == 6765)
   }
 }
